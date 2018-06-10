da4f30691bdb44baefea4fb96de05988d0079377
tinhcao <caodangtinh@gmail.com>
2018-06-10 23:44:35 +0700

    add getter

--- src/main/java/com/entity/RecordCandidate.java
+++ src/main/java/com/entity/RecordCandidate.java
@@ -14,10 +14,12 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "RECORD_CANDIDATE")
 public class RecordCandidate {
-	
+
 	@Id
 	@SequenceGenerator(name = "id_ge", sequenceName = "candidate_pk_seq")
 	@GeneratedValue(generator = "id_ge", strategy = GenerationType.SEQUENCE)
@@ -28,24 +30,55 @@ public class RecordCandidate {
 
 	@Column(name = "LAST_ACCESS")
 	private String lastAccess;
-	
-	
+
 	@ManyToOne
 	@JoinColumn(name = "USERNAME")
 	private User user;
-	
-	
-	 @OneToMany
-	 @JoinColumn(name="JOB_TITLE")
-	 private Set<Jobs> jobs = new HashSet<>();
-	 
-	 @Column(name="CONTENT")
-	 private String content;
-	 
-	 @Column(name="STATUS")
-	 private int status;
-	 
-	 @Column(name="CV_LINK")
-	 private String cvLink;
+
+	@OneToMany
+	@JoinColumn(name = "JOB_TITLE")
+	@JsonIgnore
+	private Set<Jobs> jobs = new HashSet<>();
+
+	@Column(name = "CONTENT")
+	private String content;
+
+	@Column(name = "STATUS")
+	private int status;
+
+	@Column(name = "CV_LINK")
+	private String cvLink;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getCreatedTime() {
+		return createdTime;
+	}
+
+	public String getLastAccess() {
+		return lastAccess;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Set<Jobs> getJobs() {
+		return jobs;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getCvLink() {
+		return cvLink;
+	}
 
 }
